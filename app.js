49a75724f5aa89f15a6dd8633060cd52a34c152a
SB J <>
2020-03-09 17:37:22 +0900

    painting function

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx= canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
 
 let painting = false;
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+function startPaintg(){
+    painting = true;
+}
+
 
 function stopPainting(){
     painting = false;
@@ -9,6 +21,17 @@ function stopPainting(){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function clearCanvas(event){
+    
 }
 
 function onMousedown(event){
@@ -21,7 +44,7 @@ function onMouseUp(event){
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onmousedown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPaintg);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
